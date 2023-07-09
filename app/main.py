f6d61355db9f7ad9a33c80edd2103b7d2da0d99c
Beda Schmid <beda@tukutoi.com>
2023-07-09 14:32:39 +0700

    modified:   app/main.py modified:   html/script.js

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import os
+import openai
 from dotenv import load_dotenv
-import requests
-import json
-import html
 import subprocess
+import re
+
 
 # Ask for a prompt
 prompt = input("Enter prompt:" )
@@ -15,62 +15,58 @@ base_url = os.getenv("BASE_URL")
 model = os.getenv("MODEL")
 max_tokens = int(os.getenv("MAX_TOKENS"))
 temp = float(os.getenv("TEMPERATURE"))
-token = os.getenv("TOKEN")
+top_p = float(os.getenv("TOP_P"))
+freq = float(os.getenv("FREQ_PENALTY"))
+pres = float(os.getenv("PRES_PENALTY"))
+openai.api_key = os.getenv("TOKEN")
 wdir = os.getenv("WORKING_DIR")
 
-def route_endpoint_data():
-    if model == "text-davinci-003":
-        endpoint = "completions"
-        payload = json.dumps({
-            "model": model,
-			"prompt": prompt,
-			"max_tokens": max_tokens,
-			"temperature": temp
-		})
-    else:
-        endpoint = "chat/completions"
-        payload = json.dumps({
-            "model": model,
-			"messages": [
-                {
-                    "role": "system", 
-                    "content": "You are a Professional Full Stack Developer. Provide clean, safe, secure and performant code. Do not add the question, or explanations, to your answer. Provide only pure code answers. Do not wrap the code in any backtics, or else code formatting wrappers. Provide raw, secure source codee only."
-                },
-                {
-                    "role": "user", 
-                    "content": prompt
-                }
-        	],
-            "temperature": temp,
-    		"max_tokens": max_tokens
-		})
-    return endpoint, payload
+def get_response(messages,result):
+    if model.startswith("gpt-"):
+        try:
+            response = openai.ChatCompletion.create(
+				model=model,
+				messages=messages,
+				temperature=temp,
+				max_tokens=max_tokens,
+				top_p=top_p,
+				frequency_penalty=freq,
+				presence_penalty=pres,
+                stop=["###"]
+			)
+            last_result = response['choices'][0]['message']['content']
+            result += last_result
+            stop = response['choices'][0]['finish_reason']
+            if (str(stop) == "stop"):
+                return result
+            if str(stop) == "length":
+                new_messages = [
+					{"role": "assistant", "content": last_result},
+					{"role": "user", "content": "continue"}
+				]
+                messages.extend(new_messages)
+                result = get_response(messages, result)
+                return result
+            else:
+                return False
 
-def make_post_request():
-    endpoint, payload = route_endpoint_data()
-    url = f"{base_url}/{endpoint}"
-    headers = {
-		'Content-Type': 'application/json',
-		'Authorization': f"Bearer {token}"
-	}
-    response = requests.request("POST", url, headers=headers, data=payload)
-    return response
-
-def get_response():
-    response_data = make_post_request()
-    if response_data.status_code == 200:
-        data = response_data.json()
-        choices = data.get("choices", [])
-        for choice in choices:
-                finish_reason = choice.get("finish_reason", "")
-                if model == 'text-davinci-003':
-                    answer = html.unescape(choice.get("text", "")).strip()
-                else:
-                    answer = html.unescape(choice.get("message", "")['content']).strip()
-                return answer
+        except Exception as e:
+            result, stop = f'Request failed with an exception', {e}
     else:
-       print(f"Bad response code: {response_data.status_code}")
-       return False
+        try:
+            response = openai.Completion.create(
+				model=model,
+				prompt=prompt,
+				temperature=temp,
+				max_tokens=max_tokens,
+				top_p=top_p,
+				frequency_penalty=freq,
+				presence_penalty=pres,
+				stop=["###"]
+			)
+            result, stop = response['choices'][0]['text'], response['choices'][0]['finish_reason']
+        except Exception as e:
+            result = f'Request failed with exception {e}'
 
 def git_push():
     os.chdir(wdir)
@@ -78,12 +74,27 @@ def git_push():
     subprocess.run(["git", "commit", "-m", prompt])
     subprocess.run(["git", "push", "origin", "main"])
 
-def deploy(file):
-    file_path = f"{wdir}/{file}"
-    content = get_response()
+def deploy():
+    messages = [
+		{"role": "system", "content": "You are a full stack developer. You do not wrap your code in markup, you do not use backticks or language syntax wrappers. you do not explain your response, you do not include the question in your response, you do not write a conclusion. you write only code."},
+		{"role": "user", "content": prompt + "### 1. Filename: filename ```code```"}
+	]
+    content = get_response(messages, result='')
+    print(content)
     if content != False:
-        with open(file_path, "w") as file:
-            file.write(content)
+        files = re.findall(r'(\d+\. Filename:.*?)(?=\d+\. Filename:|$)', content, re.DOTALL)
+        print(files)
+        for file in files:
+            f_r = r'Filename:\s+(.*?)\n'
+            c_r = r'```[^\n]+\n(.*?)```'
+            fname = re.search(f_r, file)
+            c = re.search(c_r, file, re.DOTALL)
+            if fname and c:
+                filename = fname.group(1)
+                code = c.group(1)
+                file_path = f"{wdir}/{filename}"
+                with open(file_path, "w") as u_file:
+                    u_file.write(code)
         git_push()
 
-deploy("index.php")
+deploy()
--- html/script.js
+++ html/script.js
@@ -5,3 +5,23 @@ $(document).ready(function() {
         var name = $('#name').val();
         var email = $('#email').val();
         var message = $('#message').val
+        $.ajax({
+            url: 'submit.php',
+            type: 'POST',
+            data: {
+                name: name,
+                email: email,
+                message: message
+            },
+            success: function(response) {
+                alert('Form submitted successfully!');
+                $('#name').val('');
+                $('#email').val('');
+                $('#message').val('');
+            },
+            error: function(xhr, status, error) {
+                alert('An error occurred while submitting the form. Please try again.');
+            }
+        });
+    });
+});
