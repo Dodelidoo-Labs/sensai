3159a206e1f11391890746999daa3d96e81bedb9
Beda Schmid <beda@tukutoi.com>
2023-07-10 10:58:53 +0700

    PHP application with main page, header including styles, footer including scripts, contact page with a contact form submitted with AJAX. Create all files and all files contents.

---
+++ html/contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="contact.js"></script>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+
+    <h1>Contact Us</h1>
+
+    <form id="contact-form" method="post" action="submit.php">
+        <input type="text" name="name" placeholder="Your Name" required>
+        <input type="email" name="email" placeholder="Your Email" required>
+        <textarea name="message" placeholder="Your Message" required></textarea>
+        <button type="submit">Submit</button>
+    </form>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
---
+++ html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Application</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <?php include 'header.php'; ?>
+
+    <h1>Welcome to my PHP Application</h1>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
